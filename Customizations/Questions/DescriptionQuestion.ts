5970883adce401487cc7f2bf3a46d1307b38ab79
Pieter Vander Vennet <pietervdvn@gmail.com>
2020-07-14 15:49:42 +0200

    Small explanation for description

--- Customizations/Questions/DescriptionQuestion.ts
+++ Customizations/Questions/DescriptionQuestion.ts
@@ -7,6 +7,7 @@ export class DescriptionQuestion extends TagRenderingOptions{
         super({
             question: "Zijn er bijzonderheden die we moeten weten over dit "+category+"?<br>" +
                 "<span class='question-subtext'>Je hoeft niet te herhalen wat je net hebt aangeduid.<br/>" +
+                "Een <i>naam</i> wordt in de volgende stap gevraagd.<br/>" +
                 "Voel je vrij om dit veld over te slaan.</span>",
             freeform:{
                 key:"description:0",
