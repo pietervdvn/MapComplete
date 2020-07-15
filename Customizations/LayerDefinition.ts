58bcdbe17ae5a43eeca1fe134a83093fe739b238
Pieter Vander Vennet <pietervdvn@gmail.com>
2020-07-15 11:23:35 +0200

    Adding documentation

--- Customizations/LayerDefinition.ts
+++ Customizations/LayerDefinition.ts
@@ -16,9 +16,35 @@ export class LayerDefinition {
      * This name is shown in the 'add XXX button'
      */
     name: string;
+    /**
+     * These tags are added whenever a new point is added by the user on the map.
+     * This is the ideal place to add extra info, such as "fixme=added by MapComplete, geometry should be checked"
+     */
     newElementTags: Tag[]
+    /**
+     * Not really used anymore
+     * This is meant to serve as icon in the buttons
+     */
     icon: string;
+    /**
+     * Only show this layer starting at this zoom level
+     */
     minzoom: number;
+
+    /**
+     * This tagfilter is used to query overpass.
+     * Examples are:
+     * 
+     * new Tag("amenity","drinking_water")
+     * 
+     * or a query for bicycle pumps which have two tagging schemes:
+     * new Or([ 
+     *  new Tag("service:bicycle:pump","yes") ,
+     *  new And([
+     *      new Tag("amenity","compressed_air"), 
+     *      new Tag("bicycle","yes")])
+     *  ])
+     */
     overpassFilter: TagsFilter;
 
     /**
