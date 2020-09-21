8b533a57f53b2922d795264bab5773c624fb3637
Pieter Vander Vennet <pietervdvn@gmail.com>
2020-09-22 00:22:50 +0200

    Attempt to add JOSM-edit-button

--- InitUiElements.ts
+++ InitUiElements.ts
@@ -422,10 +422,26 @@ export class InitUiElements {
                         "<img src='./assets/pencil.svg' alt='edit here' class='small-userbadge-icon'>" +
                         "</a>"
                 }
-                return new Combine([mapComplete, reportBug, " | ", stats,  " | ",editHere]).Render();
+                let editWithJosm = ""
+                if(location !== undefined &&
+                    State.state.osmConnection !== undefined &&
+                    State.state.bm !== undefined &&
+                    State.state.osmConnection.userDetails.data.csCount >= State.userJourney.tagsVisibleAndWikiLinked){
+                    const latLng = ( State.state.bm as Basemap).map.getBounds();
+                    const top = Math.max(latLng[0][0], latLng[0][1]);
+                    const bottom = Math.min(latLng[0][0], latLng[0][1]);
+                    const left  = Math.min(latLng[1][0], latLng[1][1]);
+                    const right = Math.max(latLng[1][0], latLng[1][1]);
 
-            })
-        ));
+                    const josmLink = `http://127.0.0.1:8111/load_and_zoom?left=${left}&right=${right}top=${top}&bottom=${bottom}`
+                    editHere =
+                        `<a href='${josmLink}' target='_blank'><img src='./assets/josm_logo.svg' alt='edit here' class='small-userbadge-icon'></a>`
+                }
+                return new Combine([mapComplete, reportBug, " | ", stats,  " | ",editHere, editWithJosm]).Render();
+
+            }, [State.state.osmConnection.userDetails])
+        )
+        );
         State.state.bm = bm;
         State.state.layerUpdater = new LayerUpdater(State.state);
         const queryParam = QueryParameters.GetQueryParameter("background", State.state.layoutToUse.data.defaultBackground);
