e731640e5f044492dda1764a389f365bc55bf30f
Pieter Vander Vennet <pietervdvn@gmail.com>
2020-09-27 21:01:46 +0200

    Add retina detection

--- Utils.ts
+++ Utils.ts
@@ -98,6 +98,9 @@ export class Utils {
     }
 
     public static isRetina() : boolean{
+        if(UIElement.runningFromConsole){
+            return;
+        }
         // The cause for this line of code: https://github.com/pietervdvn/MapComplete/issues/115
         // See https://stackoverflow.com/questions/19689715/what-is-the-best-way-to-detect-retina-support-on-a-device-using-javascript
         return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)').matches)) || (window.devicePixelRatio && window.devicePixelRatio >= 2));
