6c2585b60c8c905bd0a23564ae346c93f7a7e346
Pieter Vander Vennet <pietervdvn@gmail.com>
2020-10-27 14:32:38 +0100

    Small tweaks to cyclofix: disable parkings

--- State.ts
+++ State.ts
@@ -23,7 +23,7 @@ export default class State {
     // The singleton of the global state
     public static state: State;
     
-    public static vNumber = "0.1.1a";
+    public static vNumber = "0.1.1b";
     
     // The user journey states thresholds when a new feature gets unlocked
     public static userJourney = {
