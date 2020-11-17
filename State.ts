de305ecab9459db603a14ea43a7ae446a02f1dc8
Pieter Vander Vennet <pietervdvn@gmail.com>
2020-11-17 18:43:49 +0100

    Version bump

--- State.ts
+++ State.ts
@@ -23,7 +23,7 @@ export default class State {
     // The singleton of the global state
     public static state: State;
 
-    public static vNumber = "0.1.3-rc5";
+    public static vNumber = "0.2.0";
 
     // The user journey states thresholds when a new feature gets unlocked
     public static userJourney = {
