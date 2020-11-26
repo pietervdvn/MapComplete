f2d9eb89924c8529fb71223b9ffe12b05f44adbb
Pieter Vander Vennet <pietervdvn@gmail.com>
2020-11-26 21:44:55 +0100

    Merge upstream

--- State.ts
+++ State.ts
@@ -23,7 +23,7 @@ export default class State {
     // The singleton of the global state
     public static state: State;
 
-    public static vNumber = "0.2.2f";
+    public static vNumber = "0.2.2g";
 
     // The user journey states thresholds when a new feature gets unlocked
     public static userJourney = {
