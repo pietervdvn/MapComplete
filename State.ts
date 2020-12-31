d225a32094ff5e6cf299583f1b13071677e06526
pietervdvn <pietervdvn@posteo.net>
2020-12-31 21:14:37 +0100

    Readd version number - oops

--- State.ts
+++ State.ts
@@ -25,6 +25,7 @@ export default class State {
     // The singleton of the global state
     public static state: State;
 
+    public static vNumber = "0.2.6";
 
     // The user journey states thresholds when a new feature gets unlocked
     public static userJourney = {
