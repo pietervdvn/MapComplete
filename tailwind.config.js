f07e595830de36b2f54f76f181871e86606d8965
Tobias <t@tobiasjordans.de>
2021-01-17 21:01:44 +0100

    Add TailwindCSS
    Following https://tailwindcss.com/docs/installation

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    // './**/*.html',
+    // './**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
