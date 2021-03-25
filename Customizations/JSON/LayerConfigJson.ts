53e70b9a9c54a67a05fd7e89b16ca59589ad1ad2
pietervdvn <pietervdvn@posteo.net>
2021-03-26 00:55:36 +0100

    Small comment update

--- Customizations/JSON/LayerConfigJson.ts
+++ Customizations/JSON/LayerConfigJson.ts
@@ -55,7 +55,9 @@ export interface LayerConfigJson {
 
     /**
      * This tagrendering should either be 'yes' or 'no'. If 'no' is returned, then the feature will be hidden from view.
-     * This is useful to hide certain features from view
+     * This is useful to hide certain features from view. Important: hiding features does not work dynamically, but is only calculated when the data is first renders.
+     * This implies that it is not possible to hide a feature after a tagging change
+     * 
      * The default value is 'yes'
      */
     isShown?: TagRenderingConfigJson;
