75f33ee58035f15b2ac07f0bd9bea823b960db4a
pietervdvn <pietervdvn@posteo.net>
2021-03-29 14:10:20 +0200

    -Nuke the localstorage if it is full

--- Models/Constants.ts
+++ Models/Constants.ts
@@ -2,7 +2,7 @@ import { Utils } from "../Utils";
 
 export default class Constants {
     
-    public static vNumber = "0.6.3";
+    public static vNumber = "0.6.3a";
 
     // The user journey states thresholds when a new feature gets unlocked
     public static userJourney = {
