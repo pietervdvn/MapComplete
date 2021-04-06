117c62e01139283c7a29d7ce4fb3e4f535225858
pietervdvn <pietervdvn@posteo.net>
2021-04-06 20:00:18 +0200

    Version bump

--- Models/Constants.ts
+++ Models/Constants.ts
@@ -2,7 +2,7 @@ import { Utils } from "../Utils";
 
 export default class Constants {
     
-    public static vNumber = "0.6.6-rc-e";
+    public static vNumber = "0.6.6";
 
     // The user journey states thresholds when a new feature gets unlocked
     public static userJourney = {
