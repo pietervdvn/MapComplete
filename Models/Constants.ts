20386b7c0b4dfeb0d5364ac5d3419310c61f250a
pietervdvn <pietervdvn@posteo.net>
2021-04-23 12:59:09 +0200

    Styling tweaks for speelplekken theme

--- Models/Constants.ts
+++ Models/Constants.ts
@@ -2,7 +2,7 @@ import { Utils } from "../Utils";
 
 export default class Constants {
     
-    public static vNumber = "0.6.11";
+    public static vNumber = "0.6.11a";
 
     // The user journey states thresholds when a new feature gets unlocked
     public static userJourney = {
