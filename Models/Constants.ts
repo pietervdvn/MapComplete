041c6eab352a4d00cb2bdeacce1bd12d7fd12149
pietervdvn <pietervdvn@posteo.net>
2021-05-18 00:45:28 +0200

    Add ground floor and first floor as options in defibrillator theme

--- Models/Constants.ts
+++ Models/Constants.ts
@@ -2,7 +2,7 @@ import { Utils } from "../Utils";
 
 export default class Constants {
     
-    public static vNumber = "0.7.2k";
+    public static vNumber = "0.7.2l";
 
     // The user journey states thresholds when a new feature gets unlocked
     public static userJourney = {
