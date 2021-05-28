cdfcc0c5787f326943f134fddf4cb8d38f5e2a0a
pietervdvn <pietervdvn@posteo.net>
2021-05-28 13:33:50 +0200

    Add path

--- scripts/deployIfChanged.sh
+++ scripts/deployIfChanged.sh
@@ -4,6 +4,7 @@
 # To run with crontab: 
 # */1 * * * * /home/pietervdvn/git/MapComplete/scripts/deployIfChanged.sh >> /home/pietervdvn/auto_deploy_caching.log 2>&1
 
+PATH=/home/pietervdvn/.local/bin:/home/pietervdvn/.nvm/versions/node/v16.0.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/pietervdvn/.dotnet/tools
 
  ACTION='\033[1;90m'
  FINISHED='\033[1;96m'
