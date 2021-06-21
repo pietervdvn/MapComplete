50827d38a8bde17d50258711bb4ff63f83ddb02c
pietervdvn <pietervdvn@posteo.net>
2021-06-21 03:11:00 +0200

    Add a few comments

--- Logic/FeatureSource/MetaTaggingFeatureSource.ts
+++ Logic/FeatureSource/MetaTaggingFeatureSource.ts
@@ -3,14 +3,18 @@ import {UIEventSource} from "../UIEventSource";
 import State from "../../State";
 import Hash from "../Web/Hash";
 import MetaTagging from "../MetaTagging";
-import ExtractRelations from "../Osm/ExtractRelations";
-import FeatureSourceMerger from "./FeatureSourceMerger";
 
 export default class MetaTaggingFeatureSource implements FeatureSource {
     public readonly features: UIEventSource<{ feature: any; freshness: Date }[]> = new UIEventSource<{ feature: any; freshness: Date }[]>(undefined);
 
     public readonly name;
 
+    /***
+     * Constructs a new metatagger which'll calculate various tags
+     * @param allFeaturesSource: A source where all the currently known features can be found - used to calculate overlaps etc
+     * @param source: the source of features that should get their metatag and which should be exported again
+     * @param updateTrigger
+     */
     constructor(allFeaturesSource: FeatureSource, source: FeatureSource, updateTrigger?: UIEventSource<any>) {
         const self = this;
         this.name = "MetaTagging of " + source.name
