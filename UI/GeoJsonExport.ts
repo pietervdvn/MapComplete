bd07eed4824afb03d09ebf98c2fab4ce7a8b7f91
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2021-07-15 09:58:17 +0200

    Remove MapComplete metadata from featurelist

--- UI/GeoJsonExport.ts
+++ UI/GeoJsonExport.ts
@@ -1,14 +1,31 @@
 import FeaturePipeline from "../Logic/FeatureSource/FeaturePipeline";
 import {Utils} from "../Utils";
 
-export function exportAsGeoJson(featurePipeline: FeaturePipeline, options?: {metadata?: boolean}) {
+export function exportAsGeoJson(featurePipeline: FeaturePipeline, options?: { metadata?: boolean }) {
     let defaults = {
         metadata: false
     }
     options = Utils.setDefaults(options, defaults);
     // Select all features, ignore the freshness and other data
-    // TODO: Remove mapcomplete metadata (starting with underscore)
-    let featureList: JSON[] = featurePipeline? featurePipeline.features.data.map((feature) => feature.feature) : ["I'm empty"];
+    let featureList: JSON[] = featurePipeline ? featurePipeline.features.data.map((feature) => feature.feature) : ["I'm empty"];
+
+    function removeMetaData(featureList: JSON[]) {
+        for (let i=0; i < featureList.length; i++) {
+            let feature = featureList[i];
+            for (let property in feature.properties) {
+                if (property[0] == "_") {
+                    delete featureList[i]["properties"][property];
+                }
+            }
+        }
+        return featureList;
+    }
+
+    // Remove the metadata of MapComplete (all properties starting with an underscore)
+    if (!options.metadata) {
+        removeMetaData(featureList);
+    }
+
     let geojson = {type: "FeatureCollection", features: featureList}
 
     Utils.offerContentsAsDownloadableFile(JSON.stringify(geojson), "Geodata.json");
