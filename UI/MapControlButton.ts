bd1b29e3448076a56de4270310e75bf708ad0620
Ward <wardbeyens99@gmail.com>
2021-07-19 16:23:13 +0200

    new color and icon for navigation

--- UI/MapControlButton.ts
+++ UI/MapControlButton.ts
@@ -5,11 +5,11 @@ import Combine from "./Base/Combine";
  * A button floating above the map, in a uniform style
  */
 export default class MapControlButton extends Combine {
-    
-    constructor(contents: BaseUIElement) {
-        super([contents]);
-        this.SetClass("relative block rounded-full w-10 h-10 p-1 pointer-events-auto z-above-map subtle-background")
-        this.SetStyle("box-shadow: 0 0 10px var(--shadow-color);");
-    }
-    
+  constructor(contents: BaseUIElement) {
+    super([contents]);
+    this.SetClass(
+      "relative block rounded-full w-10 h-10 p-1 pointer-events-auto z-above-map subtle-background"
+    );
+    this.SetStyle("box-shadow: 0 0 10px var(--shadow-color);");
+  }
 }
