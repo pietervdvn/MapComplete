ea54e0f7334fd6ffcc033f1178efbba69f6a0638
pietervdvn <pietervdvn@posteo.net>
2021-08-19 18:08:39 +0200

    Theme cleanup: removed some testing themes which should not be exposed to the public, fixed a few details in others

--- Models/Constants.ts
+++ Models/Constants.ts
@@ -2,7 +2,7 @@ import { Utils } from "../Utils";
 
 export default class Constants {
     
-    public static vNumber = "0.9.0-rc3";
+    public static vNumber = "0.9.0";
 
     // The user journey states thresholds when a new feature gets unlocked
     public static userJourney = {
