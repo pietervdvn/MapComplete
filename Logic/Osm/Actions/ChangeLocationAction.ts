0a3eb966c1229773f4f3c409e9ca3aff5abb37b0
pietervdvn <pietervdvn@posteo.net>
2021-10-14 01:16:38 +0200

    Add moveLocation change description

--- Logic/Osm/Actions/ChangeLocationAction.ts
+++ Logic/Osm/Actions/ChangeLocationAction.ts
@@ -3,15 +3,40 @@ import OsmChangeAction from "./OsmChangeAction";
 import {Changes} from "../Changes";
 
 export default class ChangeLocationAction extends OsmChangeAction {
+    private readonly _id: number;
+    private readonly _newLonLat: [number, number];
+    private readonly _meta: { theme: string; reason: string };
+
     constructor(id: string, newLonLat: [number, number], meta: {
         theme: string,
         reason: string
     }) {
         super();
+        if (!id.startsWith("node/")) {
+            throw "Invalid ID: only 'node/number' is accepted"
+        }
+        this._id = Number(id.substring("node/".length))
+        this._newLonLat = newLonLat;
+        this._meta = meta;
         throw "TODO"
     }
 
-    protected CreateChangeDescriptions(changes: Changes): Promise<ChangeDescription[]> {
-        return Promise.resolve([]);
+    protected async CreateChangeDescriptions(changes: Changes): Promise<ChangeDescription[]> {
+
+        const d: ChangeDescription = {
+            changes: {
+                lat: this._newLonLat[1],
+                lon: this._newLonLat[0]
+            },
+            type: "node",
+            id: this._id, meta: {
+                changeType: "move",
+                theme: this._meta.theme,
+                specialMotivation: this._meta.reason
+            }
+
+        }
+
+        return [d]
     }
 }
