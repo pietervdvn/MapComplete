134be7177ebf5fe98d6f6f0f8ce7ddc3a8602274
pietervdvn <pietervdvn@posteo.net>
2021-11-16 02:59:33 +0100

    Use 4 spaces in translation files for compatibility with hosted weblate

--- scripts/generateTranslations.ts
+++ scripts/generateTranslations.ts
@@ -245,7 +245,7 @@ function generateTranslationsObjectFrom(objects: { path: string, parsed: { id: s
         let json = tr.toJson(lang)
         try {
 
-            json = JSON.stringify(JSON.parse(json), null, "  ");
+            json = JSON.stringify(JSON.parse(json), null, "    ");
         } catch (e) {
             console.error(e)
         }
