c998cc5c7fe300072be3fbb829eb20c8c03abff3
pietervdvn <pietervdvn@posteo.net>
2022-01-06 20:54:00 +0100

    Resize all SVG images

--- scripts/resizeSvg.sh
+++ scripts/resizeSvg.sh
@@ -7,8 +7,22 @@ resizeFile(){
     PTH="$(dirname "${1}")"
     FILE="$(basename "${1}")"
     echo "Path is $PTH, name is $FILE"
-    svg-resizer -f -x 500 -y 500 -o /tmp/resized $1
-    mv "/tmp/resized/$FILE" "$PTH"
+    
+
+    
+    if grep --quiet radialGradient $1
+    then
+        echo "SKIPPING $1: it has a radialGradient"
+    else
+        
+        if grep --quiet linearGradient $1
+        then
+                echo "WARNING for $1: it has a linearGradient"
+        fi
+    
+        svg-resizer -f -x 500 -y 500 -o /tmp/resized $1
+        mv "/tmp/resized/$FILE" "$PTH"
+    fi
 }
 
 export -f resizeFile
