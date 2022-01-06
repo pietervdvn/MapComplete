8435fb883f8c87ae0c56e547ee68683d89ea1d00
pietervdvn <pietervdvn@posteo.net>
2022-01-06 20:40:05 +0100

    Add script to automatically rescale SVG images - important to export them to PNGs

--- scripts/resizeSvg.sh
+++ scripts/resizeSvg.sh
@@ -8,7 +8,7 @@ resizeFile(){
     FILE="$(basename "${1}")"
     echo "Path is $PTH, name is $FILE"
     svg-resizer -f -x 500 -y 500 -o /tmp/resized $1
-    # mv "/tmp/resized/$FILE" "$PTH"
+    mv "/tmp/resized/$FILE" "$PTH"
 }
 
 export -f resizeFile
