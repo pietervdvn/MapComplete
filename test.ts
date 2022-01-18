057b4a31927453eab7f15308179c2546f4236e0c
pietervdvn <pietervdvn@posteo.net>
2022-01-18 02:26:21 +0100

    Improve URL handling: typing http(s) is not needed anymore for URLs

--- test.ts
+++ test.ts
@@ -1,2 +1,6 @@
-import NewNoteUi from "./UI/Popup/NewNoteUi";
-import {UIEventSource} from "./Logic/UIEventSource";
+import ValidatedTextField from "./UI/Input/ValidatedTextField";
+import {VariableUiElement} from "./UI/Base/VariableUIElement";
+
+const tf = ValidatedTextField.InputForType("url")
+tf.AttachTo("maindiv")
+new VariableUiElement(tf.GetValue()).AttachTo("extradiv")
