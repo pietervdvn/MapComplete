8b46a7e05228992bbb61d4f530ce89be9b30d48b
pietervdvn <pietervdvn@posteo.net>
2022-01-27 00:46:50 +0100

    Regenerate schemes

--- Docs/Schemas/AndOrTagConfigJsonJSC.ts
+++ Docs/Schemas/AndOrTagConfigJsonJSC.ts
@@ -1,37 +1,37 @@
 export default {
-    "$ref": "#/definitions/AndOrTagConfigJson",
-    "definitions": {
-        "AndOrTagConfigJson": {
-            "type": "object",
-            "properties": {
-                "and": {
-                    "type": "array",
-                    "items": {
-                        "anyOf": [
-                            {
-                                "$ref": "#/definitions/AndOrTagConfigJson"
-                            },
-                            {
-                                "type": "string"
-                            }
-                        ]
-                    }
-                },
-                "or": {
-                    "type": "array",
-                    "items": {
-                        "anyOf": [
-                            {
-                                "$ref": "#/definitions/AndOrTagConfigJson"
-                            },
-                            {
-                                "type": "string"
-                            }
-                        ]
-                    }
-                }
-            }
+  "$ref": "#/definitions/AndOrTagConfigJson",
+  "definitions": {
+    "AndOrTagConfigJson": {
+      "type": "object",
+      "properties": {
+        "and": {
+          "type": "array",
+          "items": {
+            "anyOf": [
+              {
+                "$ref": "#/definitions/AndOrTagConfigJson"
+              },
+              {
+                "type": "string"
+              }
+            ]
+          }
+        },
+        "or": {
+          "type": "array",
+          "items": {
+            "anyOf": [
+              {
+                "$ref": "#/definitions/AndOrTagConfigJson"
+              },
+              {
+                "type": "string"
+              }
+            ]
+          }
         }
-    },
-    "$schema": "http://json-schema.org/draft-07/schema#"
+      }
+    }
+  },
+  "$schema": "http://json-schema.org/draft-07/schema#"
 }
