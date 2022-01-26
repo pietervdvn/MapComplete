e547654b2e95590ed0c2e522bf173520d6e0e37d
pietervdvn <pietervdvn@posteo.net>
2022-01-26 21:46:04 +0100

    New stats

--- Docs/Tools/cache_centerpoints.sh
+++ Docs/Tools/cache_centerpoints.sh
@@ -6,7 +6,7 @@ cd ../..
 cd ../MapComplete-data
 git pull
 cd -
-ts-node scripts/slice.ts Docs/Tools/centerpoints.geojson 8 ../MapComplete-data/mapcomplete-changes/
+ts-node scripts/slice.ts ./Docs/Tools/centerpoints.geojson 8 ../MapComplete-data/mapcomplete-changes/
 cd -
 git add mapcomplete-changes/*
 git commit -am "New changeset data"
