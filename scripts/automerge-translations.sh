7a993eb702f86bf5502df84541115d24a74e3ed2
pietervdvn <pietervdvn@posteo.net>
2022-06-03 02:02:14 +0200

    Improve automerge script

--- scripts/automerge-translations.sh
+++ scripts/automerge-translations.sh
@@ -2,17 +2,15 @@
 
 # Automerge translations automatically fetches the translations from weblate
 
-git remote update weblate-github
-git merge --no-commit weblate-github/weblate-mapcomplete-layers
-git merge --no-commit weblate-github/weblate-mapcomplete-layer-translations
-git merge --no-commit weblate-github/weblate-mapcomplete-core
+git fetch weblate-github
+git merge --no-commit weblate-github/weblate-mapcomplete-layers weblate-github/weblate-mapcomplete-layer-translations weblate-github/weblate-mapcomplete-core
 
 npm run generate:translations
  if [ "$?" = "0" ]; then
   # Translation generation went fine - commit
   git add langs/
   git add assets/
-  git commit -m "Merge weblate translations and regenerate translations"
+  git commit -m "Merge weblate trangsslations and regenerate translations"
  else
   echo "Generation of translations failed!"
   git checkout HEAD
