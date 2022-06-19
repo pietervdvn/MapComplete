6e93ab0be6b8a99927eeb0a5a8719581d1d285aa
pietervdvn <pietervdvn@posteo.net>
2022-06-20 01:58:20 +0200

    Formatting

--- test/UI/ValidatedTextFieldTranslations.ts
+++ test/UI/ValidatedTextFieldTranslations.ts
@@ -5,13 +5,13 @@ import ValidatedTextField from "../../UI/Input/ValidatedTextField";
 import {fail} from "assert";
 
 describe("ValidatedTextFields", () => {
-    
-        it("should all have description in the translations", () => {
-            const ts = Translations.t.validation;
-            const missingTranslations = Array.from(ValidatedTextField.allTypes.keys())
-                .filter(key => ts[key] === undefined || ts[key].description === undefined)
-            if(missingTranslations !== []){
-              fail("undefined", "a `description` for "+missingTranslations.join(", ") , "These validated text fields don't have a type name defined in en.json. (Did you just add one? Run `npm run generate:translations`)")
-            }
-        })
+
+    it("should all have description in the translations", () => {
+        const ts = Translations.t.validation;
+        const missingTranslations = Array.from(ValidatedTextField.allTypes.keys())
+            .filter(key => ts[key] === undefined || ts[key].description === undefined)
+        if (missingTranslations !== []) {
+            fail("undefined", "a `description` for " + missingTranslations.join(", "), "These validated text fields don't have a type name defined in en.json. (Did you just add one? Run `npm run generate:translations`)")
+        }
+    })
 })
