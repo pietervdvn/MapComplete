98d061b24a83cc7cd221ffca72305ad78e9596fe
AlexanderRebai <alexander.rebai@student.hogent.be>
2022-07-18 14:28:12 +0000

    latest

--- test.ts
+++ test.ts
@@ -10,7 +10,9 @@ import Toggle, { ClickableToggle } from "./UI/Input/Toggle";
 const testData = ["-1", "0", "0.5", "1", "1.5", "2"]
 let slider = new Slider(0, testData.length - 1, {vertical: true});
 
-const toggleClass = "flex border-2 border-blue-500 rounded-full w-10 h-10 place-content-center items-center"
+slider.SetClass("flex m-1 elevatorslider mb-0 mt-8").SetStyle("height: "+2.5*testData.length+"rem ")
+
+const toggleClass = "flex border-2 border-blue-500 w-10 h-10 place-content-center items-center"
 
 const values = testData.map((data, i) => new ClickableToggle(
   new FixedUiElement(data).SetClass("active bg-subtle " + toggleClass), new FixedUiElement(data).SetClass(toggleClass), slider.GetValue().sync(
@@ -23,10 +25,8 @@ const values = testData.map((data, i) => new ClickableToggle(
     }
   ))
   .ToggleOnClick()
-  .SetClass("flex flex-column bg-slate-200 m-2 w-10 h-10"))
+  .SetClass("flex flex-column ml-5 bg-slate-200 w-10 h-10 valuesContainer"))
 
 const valCombine = new Combine(values.reverse())
-
-slider.SetClass("flex m-4 elevatorslider mb-0").SetStyle("height: "+3.25*testData.length+"rem")
 
 new Combine([valCombine.SetClass("mt-8"), slider]).SetClass("flex flex-row h-14").AttachTo("extradiv")
