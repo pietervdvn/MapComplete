7e010c04740808495b7f8c279118e8d690f30e24
Pieter Vander Vennet <pietervdvn@posteo.net>
2022-12-20 20:17:17 +0100

    Remove unneeded console.log

--- Logic/Osm/OsmObject.ts
+++ Logic/Osm/OsmObject.ts
@@ -70,7 +70,6 @@ export abstract class OsmObject {
 
         const url = `${OsmObject.backendURL}api/0.6/${id}`
         const rawData = await Utils.downloadJsonCachedAdvanced(url, 1000)
-        console.log(rawData)
         if (rawData["error"] !== undefined && rawData["statuscode"] === 410) {
             return "deleted"
         }
