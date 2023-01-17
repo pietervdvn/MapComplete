e1791d3acb0cb8b375296536351aad86f141d7d8
Pieter Vander Vennet <pietervdvn@posteo.net>
2023-01-17 03:37:26 +0100

    Fix the build

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,17 @@
-/** @type {import('vite').UserConfig} */
-export default {}
+const { defineConfig } = require("vite")
+import fs from "fs"
+const allHtmlFiles = fs.readdirSync(".").filter((f) => f.endsWith(".html"))
+const input = {}
+
+for (const html of allHtmlFiles) {
+  const name = html.substring(0, html.length - 5)
+  input[name] = "./" + html
+}
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input,
+    },
+  },
+})
