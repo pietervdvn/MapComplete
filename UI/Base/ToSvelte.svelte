b1b4e6acbaf6e2faa00974b79bdd9ddc15a3b3ce
wjtje <33957974+wjtje@users.noreply.github.com>
2023-02-03 14:59:08 +0100

    Cleanup ToSvelte component

--- UI/Base/ToSvelte.svelte
+++ UI/Base/ToSvelte.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
-  import BaseUIElement from "../BaseUIElement.js";
-  import { onMount } from "svelte";
+  import BaseUIElement from "../BaseUIElement.js"
+  import { onMount } from "svelte"
 
-  export let construct: BaseUIElement | (() => BaseUIElement);
-  let elem: HTMLElement;
+  export let construct: BaseUIElement | (() => BaseUIElement)
+  let elem: HTMLElement
+
   onMount(() => {
-    let html: HTMLElement
-    if (typeof construct === "function") {
-      html = construct().ConstructElement();
-    } else {
-      html = construct.ConstructElement();
-    }
+    let html =
+      typeof construct === "function"
+        ? construct().ConstructElement()
+        : construct.ConstructElement()
+
     elem.replaceWith(html)
-  });
+  })
 </script>
-<span bind:this={elem}></span>
+
+<span bind:this={elem} />
