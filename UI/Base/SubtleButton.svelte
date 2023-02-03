227551c7cbdcc0b573d68fb0aac3a7cbd485ee4b
wjtje <33957974+wjtje@users.noreply.github.com>
2023-02-03 16:11:46 +0100

    Add svelte slots to SubtleButton

--- UI/Base/SubtleButton.svelte
+++ UI/Base/SubtleButton.svelte
@@ -7,10 +7,17 @@
 
   export let imageUrl: string | BaseUIElement
   export let message: string | BaseUIElement
-  export let options: { url?: string | Store<string>; newTab?: boolean; imgSize?: string, extraClasses?: string }
+  export let options: {
+    url?: string | Store<string>
+    newTab?: boolean
+    imgSize?: string
+    extraClasses?: string
+  }
 
-  let element: HTMLElement
   let href = typeof options?.url == "string" ? options.url : ""
+
+  let imgElem: HTMLElement
+  let msgElem: HTMLElement
 
   onMount(() => {
     if (typeof options?.url != "string" && options?.url != undefined) {
@@ -19,33 +26,53 @@
       })
     }
 
-    let img: BaseUIElement
-    const imgClasses = "block justify-center flex-none mr-4 " + (options?.imgSize ?? "h-11 w-11")
-    if ((imageUrl ?? "") === "") {
-      img = undefined
-    } else if (typeof imageUrl === "string") {
-      img = new Img(imageUrl)?.SetClass(imgClasses)
-    } else {
-      img = imageUrl?.SetClass(imgClasses)
-    }
-    if (img != undefined) element.appendChild(img.ConstructElement())
+    // Image
+    if (imgElem != undefined) {
+      let img: BaseUIElement
 
-    let msg = Translations.W(message)?.SetClass("block text-ellipsis no-images flex-shrink")
-    element.appendChild(msg.ConstructElement())
+      const imgClasses = "block justify-center flex-none mr-4 " + (options?.imgSize ?? "h-11 w-11")
+      if ((imageUrl ?? "") === "") {
+        img = undefined
+      } else if (typeof imageUrl === "string") {
+        img = new Img(imageUrl)?.SetClass(imgClasses)
+      } else {
+        img = imageUrl?.SetClass(imgClasses)
+      }
+
+      if (img) imgElem.replaceWith(img.ConstructElement())
+    }
+
+    // Message
+    if (msgElem != undefined) {
+      let msg = Translations.W(message)?.SetClass("block text-ellipsis no-images flex-shrink")
+      msgElem.replaceWith(msg.ConstructElement())
+    }
   })
 </script>
 
-{#if options?.url == undefined}
-  <span bind:this={element} class="{options.extraClasses}"/>
-{:else}
-  <a {href} class="no-underline" bind:this={element} target={options?.newTab ? "_blank" : ""} />
-{/if}
+<svelte:element
+  this={options?.url == undefined ? "span" : "a"}
+  class={options.extraClasses}
+  target={options?.newTab ? "_blank" : ""}
+  {href}
+>
+  <slot name="image">
+    <template bind:this={imgElem} />
+  </slot>
+  <slot name="message">
+    <template bind:this={msgElem} />
+  </slot>
+</svelte:element>
 
 <style lang="scss">
   span,
   a {
-    @apply flex p-3 my-2 rounded-lg hover:shadow-xl transition-colors transition-shadow;
-    @apply items-center w-full;
+    @apply flex p-3 my-2 rounded-lg hover:shadow-xl transition-[color,background-color,box-shadow];
+    @apply items-center w-full no-underline;
     @apply bg-subtle text-black hover:bg-unsubtle;
+
+    :global(img) {
+      @apply block justify-center flex-none mr-4 h-11 w-11;
+    }
   }
 </style>
