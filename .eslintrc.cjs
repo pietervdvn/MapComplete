8b88c9fba94151cb9a3d7329281aaa34d66845ab
Robin van der Linde <robin.vanderlinde@gmail.com>
2023-07-27 13:03:27 +0200

    Add basic ESLint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:svelte/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    extraFileExtensions: [".svelte"],
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
+  plugins: ["@typescript-eslint"],
+  root: true,
+}
