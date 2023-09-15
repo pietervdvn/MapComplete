8c390d12fe0c3f8a5608736160e8e0c9c4865eff
Pieter Vander Vennet <pietervdvn@posteo.net>
2023-09-15 02:12:04 +0200

    Chore: cleanup debug info from sendEmail

--- src/UI/Popup/SendEmail.svelte
+++ src/UI/Popup/SendEmail.svelte
@@ -15,17 +15,8 @@
     encodeURIComponent(subject) +
     "&body=" +
     encodeURIComponent(body)
-  $: console.log(url)
-  console.log(">>> args", args)
 </script>
 <a class="button flex items-center w-full" href={url}>
   <ToSvelte construct={Svg.envelope_svg().SetClass("w-8 h-8 mr-4 shrink-0")}/>
   {button_text}
 </a>
-SEND EMAIL to {to} 
-<br/>
-subject: {subject}
-<br/>
-body: {body}
-<br/>
-{button_text}
