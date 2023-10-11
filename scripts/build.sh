3aa49b86979e71c4776562b0b25d3b6e314d9c37
Pieter Vander Vennet <pietervdvn@posteo.net>
2023-10-11 03:03:14 +0200

    CI: attempt to fix translation resources

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,8 +8,7 @@ rm -rf dist/*
 rm -rf .cache
 mkdir dist 2> /dev/null
 mkdir dist/assets 2> /dev/null
-mkdir dist/assets/langs 2> /dev/null
-mkdir dist/assets/langs/layers 2> /dev/null
+
 
 export NODE_OPTIONS="--max-old-space-size=8192"
 
@@ -52,6 +51,8 @@ vite build $SRC_MAPS
 cp -r assets/layers/ dist/assets/layers/
 cp -r assets/themes/ dist/assets/themes/
 cp -r assets/svg/ dist/assets/svg/
+mkdir dist/assets/langs 2> /dev/null
+mkdir dist/assets/langs/layers 2> /dev/null
 cp -r langs/layers/ dist/assets/langs/layers/
 ls dist/assets/langs/layers/
 export NODE_OPTIONS=""
