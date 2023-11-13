35e1a015a3f8c20c03c7b277679c27696d75fff9
Pieter Vander Vennet <pietervdvn@posteo.net>
2023-11-13 14:22:35 +0100

    Chore: prepare-deploy

--- src/assets/svg/No_checkmark.svelte
+++ src/assets/svg/No_checkmark.svelte
@@ -1,13 +1,4 @@
 <script>
-  export let color = "#000000"
+export let color = "#000000"
 </script>
-
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="374px"
-  height="259px"
-  viewBox="0 0 374 259"
-  version="1.1"
->
-  <g id="surface1" />
-</svg>
+ <svg xmlns="http://www.w3.org/2000/svg" width="374px" height="259px" viewBox="0 0 374 259" version="1.1">   <g id="surface1"/> </svg>
