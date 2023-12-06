d04132d42cee0d7a358444a925eddc48fee869d4
Pieter Vander Vennet <pietervdvn@posteo.net>
2023-12-06 03:07:15 +0100

    Make summary icons clickable

--- src/UI/BigComponents/Summary.svelte
+++ src/UI/BigComponents/Summary.svelte
@@ -10,6 +10,14 @@
   let id = feature.properties.id
   let tags = state.featureProperties.getStore(id)
   let layer: LayerConfig = state.layout.getMatchingLayer(tags.data)
+  
+  function select(){
+    state.selectedElement.setData(undefined)
+    state.selectedLayer.setData(layer)
+    state.selectedElement.setData(feature)
+  }
 </script>
 
+<div on:click={() => select()} class="cursor-pointer">
 <TagRenderingAnswer config={layer.title} selectedElement={feature} {state} {tags} {layer} />
+</div>
