32887815587d0e83d538b2cfae0d6d33cba0e0e0
Pieter Vander Vennet <pietervdvn@posteo.net>
2023-12-26 12:08:48 +0100

    UX: always center loading icon

--- src/UI/Base/Loading.svelte
+++ src/UI/Base/Loading.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class={twMerge("flex p-1 pl-2", cls)}>
-  <div class="min-w-6 h-6 w-6 animate-spin self-center">
+  <div class="min-w-6 h-6 w-6 animate-spin self-center shrink-0">
     <ToSvelte construct={Svg.loading_svg()} />
   </div>
   <div class="ml-2">
