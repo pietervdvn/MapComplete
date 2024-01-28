466d3e70ccac263a625b1f550cb85cac822032d3
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-01-28 02:52:10 +0100

    Add language selector for privacy page

--- src/UI/PrivacyGui.svelte
+++ src/UI/PrivacyGui.svelte
@@ -5,13 +5,18 @@
   import Translations from "./i18n/Translations"
   import { Utils } from "../Utils"
   import Add from "../assets/svg/Add.svelte"
+  import LanguagePicker from "./InputElement/LanguagePicker.svelte"
 </script>
 
-<div class="flex h-screen flex-col overflow-hidden p-4">
+<div class="flex h-screen flex-col overflow-hidden px-4">
+  <div class="flex justify-between">
+    
   <h2 class="flex items-center">
     <EyeIcon class="w-6 pr-2" />
     <Tr t={Translations.t.privacy.title} />
   </h2>
+    <LanguagePicker availableLanguages={Translations.t.privacy.intro.SupportedLanguages()}/>
+  </div>
   <div class="h-full overflow-auto border border-gray-500 p-4">
     <PrivacyPolicy />
   </div>
