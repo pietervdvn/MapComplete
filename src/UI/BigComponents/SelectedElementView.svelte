cae8854dbeff20ee11593a50ac68b0a59d21e3a8
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-02-20 23:47:49 +0100

    Disable feature box if the feature has changed upstream and no longer matches the layer definition

--- src/UI/BigComponents/SelectedElementView.svelte
+++ src/UI/BigComponents/SelectedElementView.svelte
@@ -17,6 +17,12 @@
   export let tags: UIEventSource<Record<string, string>> = state.featureProperties.getStore(
     selectedElement.properties.id
   )
+  
+  
+  let stillMatches = tags.map(tags => {
+    console.log("Tags are now:",tags, layer.source.osmTags?.matchesProperties(tags))
+    return !layer.source?.osmTags || layer.source.osmTags?.matchesProperties(tags)
+  })
 
   let _metatags: Record<string, string>
   onDestroy(
@@ -35,7 +41,11 @@
   )
 </script>
 
-{#if $tags._deleted === "yes"}
+{#if !$stillMatches}
+  <div class="alert"  aria-live="assertive">
+    <Tr t={Translations.t.delete.isChanged}/>
+  </div>
+{:else if $tags._deleted === "yes"}
   <div aria-live="assertive">
     <Tr t={Translations.t.delete.isDeleted} />
   </div>
