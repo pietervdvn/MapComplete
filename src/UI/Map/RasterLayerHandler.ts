894c48520a87fa3d7e15987efcec8ce3830b5325
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-02-21 18:47:12 +0100

    Add items-with-image theme

--- src/UI/Map/RasterLayerHandler.ts
+++ src/UI/Map/RasterLayerHandler.ts
@@ -71,7 +71,7 @@ class SingleBackgroundHandler {
             this.fadeOut()
         } else {
             this._deactivationTime = undefined
-            this.enable()
+            await this.enable()
             this.fadeIn()
         }
     }
@@ -85,10 +85,23 @@ class SingleBackgroundHandler {
         }
     }
 
-    private enable() {
+    private async enable(){
+        let ttl = 15
+        await this.awaitStyleIsLoaded()
+        while(!this.tryEnable() && ttl > 0){
+            ttl --;
+            await Utils.waitFor(250)
+        }
+    }
+
+    /**
+     * Returns 'false' if should be attempted again
+     * @private
+     */
+    private tryEnable(): boolean {
         const map: MLMap = this._map.data
         if (!map) {
-            return
+            return true
         }
         const background = this._targetLayer.properties
         console.debug("Enabling", background.id)
@@ -101,8 +114,7 @@ class SingleBackgroundHandler {
             try {
                 map.addSource(background.id, RasterLayerHandler.prepareWmsSource(background))
             } catch (e) {
-                console.error("Could not add source", e)
-                return
+                return false
             }
         }
         if (!map.getLayer(background.id)) {
@@ -126,6 +138,7 @@ class SingleBackgroundHandler {
                 map.setPaintProperty(background.id, "raster-opacity", o)
             })
         }
+        return true
     }
 
     private fadeOut() {
