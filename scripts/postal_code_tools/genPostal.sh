f3db4b34c9e9e78a40c03cfb46839c0243650b80
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-02-23 12:05:08 +0100

    Remove generate cache scripts

--- scripts/postal_code_tools/genPostal.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-# npm run generate:layeroverview
-cd ../..
-ts-node scripts/generateCache.ts postal_codes 8 /home/pietervdvn/Downloads/postal_codes 49.69606181911566 2.373046875 51.754240074033525 6.459960937499999 --generate-point-overview '*' --force-zoom-level 1
