94d8b18166161cd67d9f27650c7ec9569e123303
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-03-25 03:05:00 +0100

    Add sunny theme for protomaps

--- src/UI/Test.svelte
+++ src/UI/Test.svelte
@@ -1,4 +1,22 @@
 <script lang="ts">
+  import MaplibreMap from "./Map/MaplibreMap.svelte"
+  import { UIEventSource } from "../Logic/UIEventSource"
+  import { MapLibreAdaptor } from "./Map/MapLibreAdaptor"
+  import { Map as MlMap } from "maplibre-gl"
+  import { AvailableRasterLayers } from "../Models/RasterLayers"
+  import { QueryParameters } from "../Logic/Web/QueryParameters"
+
+  const map = new UIEventSource<MlMap>(undefined)
+  const mla = new MapLibreAdaptor(map, {
+    location: new UIEventSource<{ lon: number; lat: number }>({
+      lon: 3.1929, lat: 51.2190
+    }),
+    zoom: UIEventSource.asFloat(QueryParameters.GetQueryParameter("z", 13+"")),
+    rasterLayer: new UIEventSource(AvailableRasterLayers.globalLayers.at(-1))
+  })
 </script>
 
-No tests
+<div class="w-full h-screen">
+  <MaplibreMap {map} />
+</div>
+
