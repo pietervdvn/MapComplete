62ad2351c9d1f318769d1895a0e0a7c7db1871ed
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-03-26 14:56:47 +0100

    More fixes to sunny

--- src/UI/Test.svelte
+++ src/UI/Test.svelte
@@ -9,7 +9,7 @@
   const map = new UIEventSource<MlMap>(undefined)
   const mla = new MapLibreAdaptor(map, {
     location: new UIEventSource<{ lon: number; lat: number }>({
-      lon: 3.1929, lat: 51.2190
+     lat: 51.0323, lon: 3.7416
     }),
     zoom: UIEventSource.asFloat(QueryParameters.GetQueryParameter("z", 13+"")),
     rasterLayer: new UIEventSource(AvailableRasterLayers.globalLayers.at(5))
