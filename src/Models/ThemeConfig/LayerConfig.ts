d848171c3d795e31a73b10d687b475de30e8f39c
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-03-28 03:05:21 +0100

    Fix: don't count layers if they are not shown by default, fix #1840

--- src/Models/ThemeConfig/LayerConfig.ts
+++ src/Models/ThemeConfig/LayerConfig.ts
@@ -159,7 +159,7 @@ export default class LayerConfig extends WithContextLoader {
         }
         this.minzoomVisible = json.minzoomVisible ?? this.minzoom
         this.shownByDefault = json.shownByDefault ?? true
-        this.doCount = json.isCounted ?? true
+        this.doCount = json.isCounted ?? this.shownByDefault ?? true
         this.forceLoad = json.forceLoad ?? false
         if (json.presets === null) json.presets = undefined
         if (json.presets !== undefined && json.presets?.map === undefined) {
