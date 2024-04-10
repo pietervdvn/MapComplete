6b37d59e889e5bb792b39730ddf7fe394a5c0efe
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-04-10 15:40:57 +0200

    Add DynLink

---
+++ src/UI/Base/DynLink.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { ImmutableStore, Store } from "../../Logic/UIEventSource"
+
+  export let text: Store<string>
+  export let href: Store<string>
+  export let classnames: Store<string> = undefined
+  export let download: Store<string> = undefined
+  export let ariaLabel: Store<string> = undefined
+
+  export let newTab: Store<boolean> = new ImmutableStore(false)
+</script>
+
+<a
+  href={$href}
+  aria-label={$ariaLabel}
+  title={$ariaLabel}
+  target={$newTab ? "_blank" : undefined}
+  download={$download}
+  class={$classnames}
+>
+  {@html $text}
+</a>
