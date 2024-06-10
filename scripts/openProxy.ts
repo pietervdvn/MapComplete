043c9a1d7df7cc41845bb316f51f0a4ea6493d0f
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-06-10 14:54:31 +0200

    Add open proxy script

---
+++ scripts/openProxy.ts
@@ -0,0 +1,33 @@
+import Script from "./Script"
+import { Server } from "./server"
+import ScriptUtils from "./ScriptUtils"
+
+class OpenProxy  extends Script{
+
+    constructor() {
+        super("Allows any MapComplete-related domain to access the open internet via the proxy. No caching is done")
+    }
+   async main(args: string[]): Promise<void> {
+       new Server(1237,{
+
+       },[
+           {
+               mustMatch: "json",
+               mimetype: "application/json",
+               handle: async (_, params) => {
+                   const url = decodeURIComponent(params.get("url"))
+                   let content = await ScriptUtils.Download(url)
+                   while(content["redirect"]){
+                       content = await ScriptUtils.Download(content["redirect"])
+                   }
+                   return content["content"]
+               }
+           }
+       ])
+    }
+
+
+
+}
+
+new OpenProxy().run()
