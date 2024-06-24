e1a47801f1bd2b878d846318911432f01c0bebc5
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-06-24 13:00:47 +0200

    Fix test.svelte

--- src/UI/Test.svelte
+++ src/UI/Test.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-import OpeningHoursVisualization from "./OpeningHours/OpeningHoursVisualization"
 </script>
 
 <main >
-  <ToSvelte construct={new OpeningHoursVisualization()}
 </main>
