277d3d075423ee35e3027b74dab8838aeab5d0ed
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-08-21 01:15:25 +0200

    Test update script

--- scripts/osm2pgsql/update.sh
+++ scripts/osm2pgsql/update.sh
@@ -8,14 +8,14 @@ mv build_db.lua ~/data/
 TIMESTAMP=$(osmium fileinfo ~/data/planet-latest.osm.pbf -g header.option.timestamp)
 DATE=$(echo $TIMESTAMP | sed "s/T.*//")
 echo $DATE
-npm run create:database -- ${DATE/T.*//}
+npm run create:database -- -- ${DATE/T.*//}
 
 cd ~/data || exit
 rm planet-latest.osm.pbf
 wget https://planet.osm.org/pbf/planet-latest.osm.pbf
 
 rm seeddb.log
-nohup osm2pgsql -O flex -S build_db.lua -s --flat-nodes=import-help-file -d postgresql://user:password@localhost:5444/osm-poi planet-latest.osm.pbf >> seeddb.log
+osm2pgsql -O flex -S build_db.lua -s --flat-nodes=import-help-file -d postgresql://user:password@localhost:5444/osm-poi planet-latest.osm.pbf >> seeddb.log
 
 # To see the progress
 # tail -f seeddb.log
