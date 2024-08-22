6530e6c3b093d178c284104a86e63ad252258ceb
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-08-22 03:16:43 +0200

    Allow to switch to previously discovered hidden themes

--- src/Logic/Geocoding/ThemeSearch.ts
+++ src/Logic/Geocoding/ThemeSearch.ts
@@ -28,7 +28,7 @@ export default class ThemeSearch implements GeocodingProvider {
         const limit = options?.limit ?? 4
         query = Utils.simplifyStringForSearch(query)
         const withMatch = ThemeSearch.allThemes
-            .filter(th => !th.hideFromOverview )
+            .filter(th => !th.hideFromOverview || this._knownHiddenThemes.data.has(th.id))
             .filter(th => th.id !== this._state.layout.id)
             .filter(th => MoreScreen.MatchesLayout(th, query))
             .slice(0, limit + 1)
