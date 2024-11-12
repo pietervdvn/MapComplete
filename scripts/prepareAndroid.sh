2e5ab741146e983104520c0095c7523b4678b710
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-11-12 12:34:20 +0100

    Android: attempt to add verified link

---
+++ scripts/prepareAndroid.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# Copy all necessary files from the 'dist' directory into dist full
+# To be executed from the `MapComplete` repo root
+echo '''
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "org.mapcomplete",
+  appName: "MapComplete",
+  webDir: "dist-full"
+};
+
+export default config;
+''' > capacitor.config.ts
+
+rm -rf dist-full
+mkdir dist-full
+cp dist/*.html dist-full/
+cp dist/*.css dist-full/
+# cp dist/*.webmanifest dist-full/ # Not needed
+cp -r dist/css dist-full/
+
+mkdir dist-full/assets
+cp dist/assets/*.js dist-full/assets
+cp dist/assets/*.svg dist-full/assets
+cp dist/assets/*.woff dist-full/assets
+cp dist/assets/*.ttf dist-full/assets
+cp dist/assets/*.png dist-full/assets
+cp dist/assets/*.json dist-full/assets
+cp dist/assets/*.css dist-full/assets
+
+cp -r dist/assets/data dist-full/assets
+cp -r dist/assets/docs dist-full/assets
+cp -r dist/assets/fonts dist-full/assets
+cp -r dist/assets/langs dist-full/assets
+cp -r dist/assets/layers dist-full/assets
+cp -r dist/assets/png dist-full/assets
+cp -r dist/assets/svg dist-full/assets
+cp -r dist/assets/templates dist-full/assets
+cp -r dist/assets/themes dist-full/assets
+
+# mkdir dist-full/assets/generated
+
+# assets/icon-only.png will be used as the app icon
+# See https://capacitorjs.com/docs/guides/splash-screens-and-icons
+npx capacitor-assets generate
+
+npx cap sync
