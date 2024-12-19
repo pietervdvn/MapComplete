48305273e52ac85667b85b69ac2f6d13b0de8d19
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-12-19 13:59:40 +0100

    Fix: fix inspector

--- src/UI/InspectorGUI.ts
+++ src/UI/InspectorGUI.ts
@@ -1,5 +1,7 @@
 import InspectorGUI from "./InspectorGUI.svelte"
 
+const target = document.getElementById("main")
+target.innerHTML = ""
 new InspectorGUI({
-    target: document.getElementById("main"),
+    target
 })
