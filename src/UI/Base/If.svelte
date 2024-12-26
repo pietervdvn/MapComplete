ceabc5573c4f7efbe684f1483c04e5184a2326bd
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-12-26 19:38:33 +0100

    Studio: fix #2316

--- src/UI/Base/If.svelte
+++ src/UI/Base/If.svelte
@@ -5,16 +5,18 @@
   /**
    * For some stupid reason, it is very hard to let {#if} work together with UIEventSources, so we wrap then here
    */
-  export let condition: Store<boolean>
-  let _c = condition.data
-  onDestroy(
-    condition.addCallback((c) => {
-      /* Do _not_ abbreviate this as `.addCallback(c => _c = c)`. This is the same as writing `.addCallback(c => {return _c = c})`, 
-    which will _unregister_ the callback if `c = true`! */
-      _c = c
-      return false
-    })
-  )
+  export let condition: Store<boolean> | undefined
+  let _c = condition?.data
+  if (condition !== undefined) {
+    onDestroy(
+      condition.addCallback((c) => {
+        /* Do _not_ abbreviate this as `.addCallback(c => _c = c)`. This is the same as writing `.addCallback(c => {return _c = c})`,
+      which will _unregister_ the callback if `c = true`! */
+        _c = c
+        return false
+      })
+    )
+  }
 </script>
 
 {#if _c}
