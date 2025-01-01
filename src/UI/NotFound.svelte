515819ee6cf7fd199f09985a323b827e913ca035
Pieter Vander Vennet <pietervdvn@posteo.net>
2025-01-01 16:50:21 +0100

    Config: improve 404-page: use signup guy image

--- src/UI/NotFound.svelte
+++ src/UI/NotFound.svelte
@@ -3,21 +3,25 @@
   import Translations from "./i18n/Translations.ts"
   import BackButton from "./Base/BackButton.svelte"
   import Not_found from "../assets/svg/Not_found.svelte"
+  import World from "../assets/svg/World.svelte"
+
   console.log("???")
 </script>
 
-  <div class="flex flex-col items-center justify-center w-full h-full p-8">
-    <Not_found class="h-32"/>
+<div class="flex flex-col items-center justify-center w-full h-full p-8">
+  <div class="flex flex-col items-center">
+    <World style="height: 200px" />
     <h1>
-    <Tr t={Translations.t.general["404"]} />
+      <Tr t={Translations.t.general["404"]} />
     </h1>
-    <BackButton
-      on:click={() => {
+  </div>
+  <BackButton
+    on:click={() => {
         window.location = "index.html"
       }}
-    >
-      <div class="flex w-full justify-center">
-        <Tr t={Translations.t.general.backToIndex} />
-      </div>
-    </BackButton>
-  </div>
+  >
+    <div class="flex w-full justify-center">
+      <Tr t={Translations.t.general.backToIndex} />
+    </div>
+  </BackButton>
+</div>
