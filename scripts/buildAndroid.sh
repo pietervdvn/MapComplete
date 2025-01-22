714bed7c514bdb3a1b9e3668050e37c568e93adb
Pieter Vander Vennet <pietervdvn@posteo.net>
2025-01-22 02:52:58 +0100

    Scripts: create build script

---
+++ scripts/buildAndroid.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# to be run from the repo root
+nvm use
+npm run generate:layeroverview
+./scripts/prepareAndroid.sh
+
+cd android
+./gradlew assemble
+
+# Output location of the apk:
+# MapComplete/android/app/build/outputs/apk/release/app-release-unsigned.apk
+scp ./app/build/outputs/apk/release/app-release-unsigned.apk hetzner:apk/mapcomplete-latest.apk
